11c4391bc6dcb81582af03120baa3894eafa0bdc
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-06-17 13:26:58 +0530

    Add insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int count = 0;
+
+void insertion_sort(int *a, int n) {
+  for (int i = 1; i < n; i++) {
+    int key = *(a + i);
+    int j = i - 1;
+    count++;
+    while (j >= 0 && *(a + j) > key) {
+      count++;
+      *(a + j + 1) = *(a + j);
+      j--;
+    }
+    *(a + j + 1) = key;
+  }
+  return;
+}
+
+int main() {
+  int n = 0;
+  printf("Enter the no of elements in the array:");
+  scanf("%d", &n);
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    printf("Enter the %dth element into the array:", i);
+    scanf("%d", a + i);
+  }
+  insertion_sort(a, n);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+  printf("Asymptotic analysis:%d\n", count);
+  return 0;
+}
