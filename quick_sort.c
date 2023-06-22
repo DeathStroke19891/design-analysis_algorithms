1ee81b0b65ed2189ad19aeb54e694b6be32d5fd6
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-06-22 23:53:02 +0530

    Fix quick_sort.c and do analysis

--- quick_sort.c
+++ quick_sort.c
@@ -3,51 +3,41 @@
 
 int count = 0;
 
-void print(int* a, int n){
-	for(int i = 0; i < n; i++){
-		printf("%d ", *(a+i));
-	}
-	printf("\n");
-}
-
 int rearrange (int* a, int n){
-	int i = 1, j = n-1;
-	int p = *(a);
-	printf("%d\n",p);
-	print(a,n);
-	while(i<j){
-		if(*(a+i)<p){
-			i++;
-		}
-		if(*(a+j)>p){
-			j--;
-		}
-		if(*(a+i)< p && *(a+j) > p){
-			int temp = *(a+i);
-			*(a+i) = *(a+j);
-			*(a+j) = temp;
-		}
-	}
-	if(j==1){
-		return j;
-	}
-	int temp = *(a);
-	*(a) = *(a+j);
-	*(a+j) = temp;
-	return j;
+  int i = 1, j = n-1;
+  int p = *(a);
+  while(i<=j){  
+    while(*(a+i)<p && i < n){
+      count++;
+      i++;
+    }
+    while(*(a+j)>p && j > -1){
+      count++;
+      j--;
+    }
+    if(i<j){
+      int temp = *(a+i);
+      *(a+i) = *(a+j);
+      *(a+j) = temp;
+    }
+  }
+  int temp = *(a);
+  *(a) = *(a+j);
+  *(a+j) = temp;
+  return j;
 }
 
-int* quick_sort(int* a, int n){	
-	if(n==1){
-		return a;
-	}
-	int p =	rearrange(a,n);
-	print(a,n);
-	printf("%d\n",p);
-	quick_sort(a, p);
-	quick_sort(a+p, n-p);
-	return a;
+int* quick_sort(int* a, int n){
+  if(n==0){
+    return a;
+  }
+  int p = rearrange(a,n);
+  count++;
+  quick_sort(a, p);
+  quick_sort(a+p+1, n-p-1);
+  return a;
 }
+
 int main() {
   int n;
   printf("Enter the number of elements in the array:");
